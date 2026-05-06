d36d917445bfc0744994cf8bbb766cb4f2664b07
JuelzC <juelzlynch06@gmail.com>
2026-05-06 08:44:57 -0400

    Sessions

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
-<<<<<<< HEAD
 use App\Http\Controllers\AdminRequestController;
-=======
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\MangaRequestController;
 use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\AddFavorite;   
 
 Route::get('/', function () {
     return view('home');
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 7a6245957a8949fcbeb936349767e2dbe59d8d07
 
 Route::get('/request-manga', [MangaRequestController::class, 'index']);
 Route::post('/request-manga', [MangaRequestController::class, 'store']);
@@ -41,13 +36,12 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/register', [RegisterController::class, 'register'])->name('register.form');
 Route::post('/register', [RegisterController::class, 'accCreate'])->name('register');
 
-<<<<<<< HEAD
 Route::get('/admin/requests', [AdminRequestController::class, 'index']);
 
 Route::post('/admin/requests/approve/{id}', [AdminRequestController::class, 'approve']);
 
 Route::post('/admin/requests/reject/{id}', [AdminRequestController::class, 'reject']);
-=======
+
 Route::get('/BlackJackVolume1Chapter1', function () {
     return app(PagesController::class)->showChapter(4); })->name('BlackJackVolume1Chapter1');
 
@@ -56,6 +50,6 @@ Route::get('/blackjack', function () {
     return view('BlackJack');
     })->name('blackjack.page');
 
-    Route::post('/favorites/add', [AuthController::class, 'addFavorite'])->name('favorites.add');
+Route::post('/favorites/add', [AddFavorite::class, 'addFavorite'])->name('favorites.add');
 
 Route::get('/user-profile', [UserProfileController::class, 'userProfile'])->name('user-profile');
